bb568b02823360c42318f82fcea30e4afe3f2572
Alessandro Carrano <alessandro.carrano@gmail.com>
2022-12-19 11:14:46 +0100

    adding output file

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+import json
+
 print ("Hello World 1")
+
+output = {
+    "db_password": {
+    "sensitive": True,
+    "value": "notasecurepassword"
+  },
+  "db_username": {
+    "sensitive": False,
+    "value": "admin"
+  }
+}
+
+print("My output:")
+print (output)
+
+with open("output/qovery-output.json", "w") as outfile:
+    json.dump(output, outfile)
